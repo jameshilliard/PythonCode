3b313b9954459a3d6393563d49d415934e284483
RoyXu <xxjbs001@126.com>
2014-03-18 02:23:12 +0800

    Add AndroidProject

---
+++ AndroidProject/s01_05_Activity/gen/com/example/s01_05_activity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.s01_05_activity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AndroidProject/s01_E05_View/gen/com/marschen/s01_e05_view/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.marschen.s01_e05_view;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
