5e1ddbefa5b266b60a0c5c2dd1d4d33dac8dfc15
AT-Roy <xxjbs001@126.com>
2014-05-24 17:08:20 +0800

    add new project DjangoTest

---
+++ DjangoTest/Demo1/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+import datetime
+
+def hello(request):
+    return HttpResponse("Hello World")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
---
+++ DjangoTest/DjangoTest/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+from Demo1.views import hello
+from Demo1.views import current_datetime
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'DjangoTest.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url('^hello/$', hello),
+    url('^time/$', current_datetime),
+    url('^another-time-page/$', current_datetime),
+)
