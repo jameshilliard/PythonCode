ef414e36033548d99c7fd50d0d7872c35ca3dfed
roy@fedora15 <zxu@actiontec.com>
2014-08-20 10:13:10 +0800

    modified:   RFS_Project_1.0/TL_WEB6000/Share_Resource.txt 	modified:   RFS_Project_1.0/library/Wifi/Wifi.py

--- RFS_Project_1.0/library/Wifi/Wifi.py
+++ RFS_Project_1.0/library/Wifi/Wifi.py
@@ -6,18 +6,23 @@ import re
 class Wifi:
 
     def __init__(self):
+        """"""
         self.wan_ip = os.getenv('G_HOST_IP1')
 
     def get_Interface(self):
+        """Get the wireless Interface on the PC such as wlan0"""
         pass
 
-    def generate_Configfle(self):
+    def generate_Configfile(self):
+        """Generate Wireless config file for wireless card to connect AP"""
         pass
 
     def scan_SSID(self):
+        """ Wireless card can scan all SSIDs to check the specified SSID exist or not"""
         pass
 
     def wifi_Status(self):
+        """ Check the connect status that Wireless card connect AP"""
         pass
 
 
