2f856d159c915d9b27859723772838b025114a8a
roy@fedora15 <zxu@actiontec.com>
2014-08-20 09:59:43 +0800

    modified:   RFS_Project_1.0/ECB6000/Wireless.txt modified:   RFS_Project_1.0/TL_WEB6000/Function_Test/Connectivities/Bridging_Functionality/Backhaul:Ethernet/Wi-Fi(2.4GHz)/IPv4.txt new file:   RFS_Project_1.0/library/Wifi/Wifi.py

---
+++ RFS_Project_1.0/library/Wifi/Wifi.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import pexpect
+import re
+
+class Wifi:
+
+    def __init__(self):
+        self.wan_ip = os.getenv('G_HOST_IP1')
+
+    def get_Interface(self):
+        pass
+
+    def generate_Configfle(self):
+        pass
+
+    def scan_SSID(self):
+        pass
+
+    def wifi_Status(self):
+        pass
+
+
