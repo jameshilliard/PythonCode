6b119b40414809fafe4127c87b17e937b8b192a0
roy <xxjbs001@126.com>
2014-12-01 01:19:13 +0800

    add  test files

--- PythonNetworkProgrammingCookbook/1_3_ip4_address_conversion.py
+++ PythonNetworkProgrammingCookbook/1_3_ip4_address_conversion.py
@@ -8,7 +8,7 @@ import socket
 from binascii import hexlify
 
 def convert_ip4_address():
-    for ip_addr in ['127.0.0.1', '192.168.0.1']:
+    for ip_addr in ['127.0.0.1', '192.168.1.8']:
         packed_ip_addr = socket.inet_aton(ip_addr)
         unpacked_ip_addr = socket.inet_ntoa(packed_ip_addr)
         print "IP Address: %s => Packed: %s, Unpacked: %s" %(ip_addr, hexlify(packed_ip_addr), unpacked_ip_addr)
