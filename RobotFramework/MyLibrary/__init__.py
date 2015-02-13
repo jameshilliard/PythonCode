1d472b5079b72a6f90998a60be4b5859fe33a7df
roy@ubuntu <zxu@actiontec.com>
2015-02-13 18:05:15 +0800

    add test cases

---
+++ RobotFramework/MyLibrary/__init__.py
@@ -0,0 +1,7 @@
+from testTemp import TestTemp
+from version import VERSION
+ 
+_version_ = VERSION
+ 
+class MyLibrary(TestTemp):
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
---
+++ RobotFramework/MyLibrary/testTemp.py
@@ -0,0 +1,6 @@
+class TestTemp(object):
+    def __init__(self):
+        pass
+ 
+    def mytest(self):
+        print "Actiontec Automation Test"
